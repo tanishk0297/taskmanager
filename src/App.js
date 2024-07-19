1690f916d0e0de43382f0170e64bfd8ec0e27882
Tanishk Shrivastava <65850602+tanishk0297@users.noreply.github.com>
2024-07-19 08:44:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,8 +30,7 @@ const App = () => {
       <h1>Task Management App</h1>
       
       {loading ? (
-        <p>Loading...</p>
-        <p>As the api is hosted over a fre tier , you may face 1 min to lodaing time after a period of inactivity</p>
+        <p>Loading the api is hosted over a free tier , you may face 1 min to lodaing time after a period of inactivity</p>
       ) : error ? (
         <p>{error}</p>
       ) : (
